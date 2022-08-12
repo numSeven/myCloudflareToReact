102575a71a331c7f5d117ed6bc1f6f35588dd53a
zhanqingyuan <zhanqingyuan@zhixueyun.com>
2022-08-13 03:12:40 +0800

    修改

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Axios from "axios"
 function App() {
   function getData(e:any) {
     // e.preventDefault();
+    // Axios.get('/api/getData',{params:{name:'aaa'}})
     Axios.get('https://mycloudflare_init.11980762821814.workers.dev/api/getData',{params:{name:'aaa'}})
   }
   function setData(e:any) {
     // e.preventDefault();
-    Axios.post('https://mycloudflare_init.11980762821814.workers.dev/api/setData',{name:'aaa',val:123})
+    // Axios.post('/api/setData',{name:'aaa',val:'dddd'})
+    Axios.post('https://mycloudflare_init.11980762821814.workers.dev/api/setData',{name:'aaa',val:'123'})
   }
   let  a = []
   console.log(a.length);
@@ -29,7 +31,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          tstst
+          tstst111
         </a>
       </header>
     </div>
